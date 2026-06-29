b694040327d7348ec29f7e6f4f2816010e18cd3a
vxmpie <marple.rblx@gmail.com>
2026-06-29 16:13:17 +0700

    upd

--- loader.lua
+++ loader.lua
@@ -1,1 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vxmpie/storage_hunter/main/src/main.lua"))()
+local url = "https://raw.githubusercontent.com/vxmpie/storage_hunter/main/src/main.lua?t=" .. tostring(tick())
+local success, code = pcall(function() return game:HttpGet(url) end)
+
+if not success then
+    warn("[Network Error] โหลดไฟล์จาก GitHub ไม่ได้ เช็กลิงก์หรืออินเทอร์เน็ต")
+else
+    local func, err = loadstring(code)
+    if not func then
+        warn("[Syntax Error] ตรวจพบจุดผิดในไฟล์ main.lua!")
+        warn("บรรทัดที่พัง: " .. tostring(err))
+    else
+        func()
+    end
+end
